ff2e0ecbb12d115b8452643a79d0c225fd09aa06
Ryan Draga <ryan.draga@boxingoctop.us>
2022-06-01 15:31:39 -0400

    Fix bug in install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,22 @@ for link in "${links[@]}"; do
   else
     ln -s $PWD/.dotfiles/$link ~/$link
   fi
+done
 
 echo "Changing Shell to ZSH..."
 chsh -s /bin/zsh
+
+read -p "Would you like to install Oh My VIM? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Installing Oh My VIM..."
+  curl -L https://raw.github.com/liangxianzhe/oh-my-vim/master/tools/install.sh | sh
+  echo "Oh My VIM Pre-Install Complete! Run Vim for the first time to complete installation."
+fi
+
+read -p "Would you like to install Powerlevel10k? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Installing Powerlevel10k..."
+  echo "export USE_POWERLEVEL10K=1" >> ~/.zshrc.d/env_vars.zsh
 
 echo "Activating New Environment..."
 /bin/zsh
