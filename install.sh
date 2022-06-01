1358261e7d988ed02fd9a03d7f011051f1e1e5a6
Ryan Draga <ryan.draga@boxingoctop.us>
2022-06-01 15:42:48 -0400

    Further bug fixes

--- install.sh
+++ install.sh
@@ -17,7 +17,11 @@ chsh -s /bin/zsh
 read -p "Would you like to install Oh My VIM? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Installing Oh My VIM..."
-  curl -L https://raw.github.com/liangxianzhe/oh-my-vim/master/tools/install.sh | sh
+  if [ -d ~/.oh-my-vim ]; then
+    echo "Oh My VIM already installed. Skipping..."
+  else
+    curl -L https://raw.github.com/liangxianzhe/oh-my-vim/master/tools/install.sh | sh
+  fi
   echo "Oh My VIM Pre-Install Complete! Run Vim for the first time to complete installation."
 fi
 
@@ -25,6 +29,7 @@ read -p "Would you like to install Powerlevel10k? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Installing Powerlevel10k..."
   echo "export USE_POWERLEVEL10K=1" >> ~/.zshrc.d/env_vars.zsh
+fi
 
 echo "Activating New Environment..."
 /bin/zsh
