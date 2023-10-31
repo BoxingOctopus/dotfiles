dd53edac3c55477aeef2de6c357da13445cb7ac0
Ryan Draga <ryan.draga@boxingoctop.us>
2023-10-31 15:47:09 -0400

    Update antigen_config.zsh

--- .zshrc.d/antigen_config.zsh
+++ .zshrc.d/antigen_config.zsh
@@ -4,12 +4,15 @@ antigen use oh-my-zsh
 # Load the bundles
 source $HOME/.zshrc.d/antigen_plugins.zsh
 
+# Disable this until there's time to fix it
 # Load the theme.
-if [ $USE_POWERLEVEL10K=1 ]; then
-    antigen theme romkatv/powerlevel10k
-else
-    antigen theme $ANTIGEN_THEME
-fi
+#if [ $USE_POWERLEVEL10K=1 ]; then
+#    antigen theme romkatv/powerlevel10k
+#else
+#    antigen theme $ANTIGEN_THEME
+#fi
+
+antigen theme romkatv/powerlevel10k
 
 # Tell Antigen that you're done.
 antigen apply
